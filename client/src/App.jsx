620fd895812c6241f92a35ab49d633c7afc08424
max <maxcamina@gmail.com>
2024-03-02 22:39:53 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
